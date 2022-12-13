fc717ae7db76e50ef042b3d50ce405ac1435553c
Dukunde Irakoze Happy <acele4444@gmail.com>
2022-12-13 12:10:11 +0200

    intergration

--- src/components/Calc.jsx
+++ src/components/Calc.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Calc.css"
-// import axios from 'axios'
+import axios from 'axios'
 import { useState } from "react";
 
 const Calc = ()=>{
         
-    const [operator1,setOperator1] = useState()
-    const [operator2,setOperator2]=useState()
-    const [operation,setOperation] = useState()
+    const [operator1,setOperator1] = useState(0)
+    const [operator2,setOperator2]=useState(0)
+    const [operation,setOperation] = useState("")
     const [result,setResult] = useState("")
 
     // onChange = (e) => {
@@ -17,31 +17,39 @@ const Calc = ()=>{
     //       };
         
     //   };
-
+    
     const data = {
         operator1: operator1,
         operator2: operator2,
         operation: operation
     }
-    
+
     const doMath = ()=>{
         axios.post("http://localhost:4040/api/calc",data)
         .then((res)=>{
+            console.log("heeree")
             console.log(res.data)
-            setResult(data)
+            setResult(res.data)
         })
         .catch((ex)=>{
-            console.log(ex)
+            console.log("Error in doMAth react!")
+            
         })
     }
+
+    // const onChange=(e)=>{
+    //     setOperator1(e.target.value)
+    //     console.log(operator1)
+    // }
+ 
 
     return(
         <div className="container">
             <div className="left">
-                <input type="number" name="operator1" id="operator1" placeholder="Enter an Operator" onChange={(e)=>{setOperator1(e.target.value)}}/>
+                <input type="number" name="operator1" id="operator1" placeholder="Enter an Operator" onChange={(e)=>{setOperator1(e.target.value);}}/>
                 <input type="number" name="operator2" id="operator2" placeholder="Enter another Operator" onChange={(e)=>{setOperator2(e.target.value)}}/>
-                <input type="text" name="operation" id="operation" placeholder="* / + -" onChange={(e)=>{setOperation(e.target.value)}}/>
-                <button onClick={()=>doMath}>DoMath!</button>
+                <input type="text" name="operation" id="operation" placeholder="* / + -" onChange={(e)=>{setOperation(e.target.value);}}/>
+                <button onClick={doMath}>DoMath!</button>
             </div>
             <div className="right">
                 <h3>Result</h3>
